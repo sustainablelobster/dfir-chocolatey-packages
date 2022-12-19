c3306848066542a14fd058c88210d1ab6c441dce
sustainablelobster <sustainablelobster@protonmail.com>
2022-12-19 11:22:01 -0800

    Remove ALEAPP zip after install

--- aleapp/tools/chocolateyinstall.ps1
+++ aleapp/tools/chocolateyinstall.ps1
@@ -5,3 +5,4 @@ $fileLocation = Join-Path $toolsDir 'ALEAPP-windows.zip'
 Get-ChocolateyUnzip $fileLocation $toolsDir
 $aleappGuiPath = (Get-ChildItem -Path $toolsDir -Filter 'aleappGUI.exe' -Recurse).FullName
 New-Item -Path ($aleappGuiPath + '.gui') -Type 'File'
+Remove-Item -Path $fileLocation -Force

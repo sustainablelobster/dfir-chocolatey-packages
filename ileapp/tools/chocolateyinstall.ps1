6e58b81cea1778d2fa31599c337df702c81e0442
sustainablelobster <sustainablelobster@protonmail.com>
2022-12-23 12:31:14 -0800

    Add ileapp package

---
+++ ileapp/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$ToolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$FileLocation = Join-Path $ToolsDir 'iLEAPP-windows.zip'
+Get-ChocolateyUnzip $fileLocation $ToolsDir
+$IleappGuiPath = (Get-ChildItem -Path $ToolsDir -Filter 'ileappGUI.exe' -Recurse).FullName
+New-Item -Path ($IleappGuiPath + '.gui') -Type 'File'
+Remove-Item -Path $fileLocation -Force
---
+++ ileapp/update.ps1
@@ -0,0 +1,44 @@
+Import-Module -Name 'au'
+
+function global:au_SearchReplace {
+    @{
+        '.\tools\VERIFICATION.txt' = @{
+            '(?i)(\s+URL32:).*'      = "`${1} $($Latest.URL32)"
+            '(?i)(\s+Checksum32:).*' = "`${1} $($Latest.Checksum32)"
+            '(?i)(\s+Get-RemoteChecksum).*' = "`${1} $($Latest.URL32)"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $SavedProgressPreference = $ProgressPreference
+    $ProgressPreference = 'SilentlyContinue'
+    $LatestReleasePage = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/abrignoni/iLEAPP/releases/latest'
+    if ($LatestReleasePage.Content -notmatch 'https://github\.com/abrignoni/iLEAPP/releases/expanded_assets/v?\.((?:\d+\.){0,3}\d+)') {
+        throw "Unable to find assets page"
+    }
+
+    $AssetsUrl = $Matches[0]
+    $Version = $Matches[1]
+
+    $AssetsPage = Invoke-WebRequest -UseBasicParsing -Uri $AssetsUrl
+    $ProgressPreference = $SavedProgressPreference
+    $URL32 = $AssetsPage.Links.href | Where-Object { $_ -match 'iLEAPP-windows\.zip' }
+    if ($null -eq $URL32) {
+        throw "Unable to find zip URL"
+    }
+    if ($URL32 -notmatch '^https://github\.com') {
+        $URL32 = 'https://github.com' + $URL32
+    }
+
+    @{
+        Version = $Version
+        URL32 = $URL32
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -NoSuffix
+}
+
+Update-Package -ChecksumFor 'none'

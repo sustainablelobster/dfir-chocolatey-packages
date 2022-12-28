3a3613233df329c624602da7b58c3b084941662e
sustainablelobster <sustainablelobster@protonmail.com>
2022-12-27 17:23:03 -0800

    Add volatility3 package

---
+++ volatility3/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$ToolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$Vol3VenvPath = Join-Path -Path $ToolsDir -ChildPath 'vol3_virtualenv'
+python -m virtualenv "$Vol3VenvPath"
+$Vol3VenvActivator = Join-Path -Path $Vol3VenvPath -ChildPath 'Scripts\activate.ps1'
+. $Vol3VenvActivator
+pip install --disable-pip-version-check volatility3==$env:ChocolateyPackageVersion
+deactivate
+$IgnorableExes = (Get-ChildItem -Path $Vol3VenvPath -Filter '*.exe' -Exclude 'vol*' -Recurse).FullName
+foreach ($Exe in $IgnorableExes) {
+    New-Item -Path "$Exe.ignore" -ItemType 'File'
+}
---
+++ volatility3/update.ps1
@@ -0,0 +1,30 @@
+Import-Module -Name 'au'
+
+function global:au_SearchReplace {
+    @{
+        '.\volatility3.nuspec' = @{
+            '^\s*<dependency id="python3" version=".+" />\s*$' = "      <dependency id=`"python3`" version=`"$($Latest.PythonVersion)`" />"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $PyPiPage = Invoke-WebRequest -UseBasicParsing -Uri 'https://pypi.org/project/volatility3'
+    
+    if ($PyPiPage.Content -notmatch 'volatility3\s+(\d+(?:\.\d+){0,3})') {
+        throw 'Failed to find version'
+    }
+    $Version = $Matches[1]
+
+    if ($PyPiPage.Content -notmatch 'Python (?:&gt;|=)=(3(?:\.\d+){0,3})') {
+        throw 'Failed to find required Python version'
+    }
+    $PythonVersion = $Matches[1]
+    
+    @{
+        Version = $Version
+        PythonVersion = $PythonVersion
+    }
+}
+
+Update-Package -ChecksumFor 'none'

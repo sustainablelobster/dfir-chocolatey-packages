ce209527f391f1e4136785503bb3e9b31c2c5949
sustainablelobster <sustainablelobster@protonmail.com>
2023-01-25 18:16:15 -0800

    Change aurora-ir title

---
+++ aurora-ir/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$ToolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ZipPath = (Get-ChildItem -Path $ToolsDir -Filter 'Aurora-win32-x64*.zip').FullName
+Get-ChocolateyUnzip -FileFullPath $ZipPath -Destination $ToolsDir
+Remove-Item -Path $ZipPath -Force
+$AuroraPath = (Get-ChildItem -Path $ToolsDir -Filter 'Aurora.exe' -Recurse).FullName
+New-Item -Path ($AuroraPath + '.gui') -Type 'File'

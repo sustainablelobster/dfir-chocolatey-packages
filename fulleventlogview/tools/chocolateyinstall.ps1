3f3d654705f787e79c9820a690d5a46b9b23c4a2
Chocolatey <chocolatey@realdimensions.net>
2023-02-15 16:02:02 +0000

    AU: 1 updated - fulleventlogview [skip ci] https://gist.github.com/9bec9a41eecea4b89447ad26780384ec/68462cde947938070f969878b00ee96085339f9a

--- fulleventlogview/tools/chocolateyinstall.ps1
+++ fulleventlogview/tools/chocolateyinstall.ps1
@@ -6,9 +6,9 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   url           = 'https://www.nirsoft.net/utils/fulleventlogview.zip'
   url64bit      = 'https://www.nirsoft.net/utils/fulleventlogview-x64.zip'
-  checksum      = '839bd243b6aee06ae77fe9bc1a5249f788f1def99a6e97fd02f9084b9ec9452c'
+  checksum      = '49023dd20359187671e76693f6bc4175c2c54cdef1503b4f64953e4761b79397'
   checksumType  = 'sha256'
-  checksum64    = 'f9f6f75fc7bd3968a28ef969e715224f1a875ccd0bf1454ef8cfbce395f03501'
+  checksum64    = '0e408f33c8d06a02b4baa1b0866f993e1376d0cbbb90a33b0528a832fad29bee'
   checksumType64= 'sha256'
 }
 

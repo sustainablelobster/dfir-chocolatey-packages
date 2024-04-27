55fb4287c0a2bd2edd529a419e2db331998c0446
sustainablelobster <sustainablelobster@protonmail.com>
2024-04-27 10:52:40 -0400

    add ezviewer

---
+++ ezviewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$ToolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ZipPath = (Get-ChildItem -Path $ToolsDir -Filter 'EZViewer.zip').FullName
+Get-ChocolateyUnzip -FileFullPath $ZipPath -Destination $ToolsDir
+Remove-Item -Path $ZipPath -Force
+$ExePath = (Get-ChildItem -Path $ToolsDir -Filter 'EZViewer.exe' -Recurse).FullName
+New-Item -Path ($ExePath + '.gui') -Type 'File'

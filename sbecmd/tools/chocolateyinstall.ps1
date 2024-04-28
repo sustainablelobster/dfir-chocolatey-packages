be92c276f09fc590a46e1ca9b14466acab7180e7
sustainablelobster <sustainablelobster@protonmail.com>
2024-04-28 10:11:33 -0400

    add sbecmd

---
+++ sbecmd/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = 'https://f001.backblazeb2.com/file/EricZimmermanTools/net6/SBECmd.zip'
+  checksum      = '76e68ea696cb94f3a26c5c05e01467bdd8e18503fabd55ce4065448251071595'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs

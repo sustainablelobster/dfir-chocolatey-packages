071a3ccba3ea92f47c6efeed6c8280371d6cbc4c
Chocolatey <chocolatey@realdimensions.net>
2024-05-02 16:02:51 +0000

    AU: 3 updated - dcode ileapp ileapp-gui [skip ci] https://gist.github.com/sustainablelobster/9bec9a41eecea4b89447ad26780384ec/58f2fbb55139a11c0f0ac4c3cf7ceedca5324acf

--- dcode/tools/chocolateyinstall.ps1
+++ dcode/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $ZipPackageArgs = @{
     PackageName   = $env:ChocolateyPackageName
     UnzipLocation = $ToolsDir
     Url           = 'https://www.digital-detective.net/download/download.php?downcode=ae2znu5994j1lforlh03'
-    Checksum      = 'dbb23d6ea4f572fbaec017fb8acc2a8b62b74fafa81ea4a388966ec14087a9e4'
+    Checksum      = '9ffe1106ee9d9f55b53d5707621d5990f493604e20f3dbdb0d22ec1b8ecb2458'
     ChecksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @ZipPackageArgs

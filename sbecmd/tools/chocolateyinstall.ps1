1f3760fdf01ababdc1233417f84954ff62751df6
sustainablelobster <sustainablelobster@protonmail.com>
2024-09-30 19:01:51 -0700

    update sbecmd

--- sbecmd/tools/chocolateyinstall.ps1
+++ sbecmd/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://f001.backblazeb2.com/file/EricZimmermanTools/net6/SBECmd.zip'
+  url           = 'https://download.ericzimmermanstools.com/net6/SBECmd.zip'
   checksum      = '76e68ea696cb94f3a26c5c05e01467bdd8e18503fabd55ce4065448251071595'
   checksumType  = 'sha256'
 }

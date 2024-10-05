d3147a001de5a3362e7cae1766e65305d90af170
sustainablelobster <sustainablelobster@protonmail.com>
2024-10-04 19:47:00 -0700

    update ileapp-gui

---
+++ ileapp-gui/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$ToolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ZipPath = (Get-ChildItem -Path $ToolsDir -Filter 'ileappGUI_Windows.zip').FullName
+Get-ChocolateyUnzip -FileFullPath $ZipPath -Destination $ToolsDir
+Remove-Item -Path $ZipPath -Force

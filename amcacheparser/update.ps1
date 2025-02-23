bae5ab92410b09859370312d34be44d72e03c46e
sustainablelobster <sustainablelobster@protonmail.com>
2025-02-23 06:45:35 -0800

    update amcacheparser to .net 9 version

--- amcacheparser/update.ps1
+++ amcacheparser/update.ps1
@@ -13,7 +13,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $LatestReleasePage = Invoke-WebRequest -UseBasicParsing `
             -Uri 'https://raw.githack.com/EricZimmerman/ericzimmerman.github.io/master/index.md'
-    if ($LatestReleasePage.Content -notmatch 'AmcacheParser.*&vert;\s*\[([\d.]+)\]\((.*/net6/AmcacheParser.zip)\)') {
+    if ($LatestReleasePage.Content -notmatch 'AmcacheParser.*&vert;\s*\[([\d.]+)\]\((.*/net9/AmcacheParser.zip)\)') {
         throw "Unable to find tool info"
     }
 

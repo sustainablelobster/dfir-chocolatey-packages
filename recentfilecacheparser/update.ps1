7ea542354c67550392f855f5f65f4872a4124300
sustainablelobster <sustainablelobster@protonmail.com>
2025-02-23 08:05:55 -0800

    upgrade recentfilecacheparser to .net 9 version

--- recentfilecacheparser/update.ps1
+++ recentfilecacheparser/update.ps1
@@ -13,7 +13,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $LatestReleasePage = Invoke-WebRequest -UseBasicParsing `
             -Uri 'https://raw.githack.com/EricZimmerman/ericzimmerman.github.io/master/index.md'
-    if ($LatestReleasePage.Content -notmatch 'RecentFileCacheParser.*&vert;\s*\[([\d.]+)\]\((.*/net6/RecentFileCacheParser.zip)\)') {
+    if ($LatestReleasePage.Content -notmatch 'RecentFileCacheParser.*&vert;\s*\[([\d.]+)\]\((.*/net9/RecentFileCacheParser.zip)\)') {
         throw "Unable to find tool info"
     }
 

6a1a71dbbe06050ed8543610bc2e01cd5a342d82
sustainablelobster <sustainablelobster@protonmail.com>
2025-02-23 08:34:58 -0800

    upgrade sbecmd to .net 9 version

--- sbecmd/tools/chocolateyinstall.ps1
+++ sbecmd/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://download.ericzimmermanstools.com/net6/SBECmd.zip'
-  checksum      = '769c0c3548efa70f81748918a7cef017f106438e9fa516e1415450ce6310f451'
+  url           = 'https://download.ericzimmermanstools.com/net9/SBECmd.zip'
+  checksum      = '797E31D3641EDE942DA2C338766587B69DCCD36D2FAA5DEEAA55C9D8174A1C4C'
   checksumType  = 'sha256'
 }
 
--- sbecmd/update.ps1
+++ sbecmd/update.ps1
@@ -13,7 +13,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $LatestReleasePage = Invoke-WebRequest -UseBasicParsing `
             -Uri 'https://raw.githack.com/EricZimmerman/ericzimmerman.github.io/master/index.md'
-    if ($LatestReleasePage.Content -notmatch 'SBECmd.*&vert;\s*\[([\d.]+)\]\((.*/net6/SBECmd.zip)\)') {
+    if ($LatestReleasePage.Content -notmatch 'SBECmd.*&vert;\s*\[([\d.]+)\]\((.*/net9/SBECmd.zip)\)') {
         throw "Unable to find tool info"
     }
 

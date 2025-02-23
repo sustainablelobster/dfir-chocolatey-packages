7010738b49cb2664ad31b3de25b9fdbc439c419c
sustainablelobster <sustainablelobster@protonmail.com>
2025-02-23 09:27:31 -0800

    upgrade vscmount to .net 9 version

--- vscmount/update.ps1
+++ vscmount/update.ps1
@@ -13,7 +13,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $LatestReleasePage = Invoke-WebRequest -UseBasicParsing `
             -Uri 'https://raw.githack.com/EricZimmerman/ericzimmerman.github.io/master/index.md'
-    if ($LatestReleasePage.Content -notmatch 'VSCMount.*&vert;\s*\[([\d.]+)\]\((.*/net6/VSCMount.zip)\)') {
+    if ($LatestReleasePage.Content -notmatch 'VSCMount.*&vert;\s*\[([\d.]+)\]\((.*/net9/VSCMount.zip)\)') {
         throw "Unable to find tool info"
     }
 

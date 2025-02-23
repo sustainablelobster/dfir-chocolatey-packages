5e0b47e22ca37e00866c030e69d95e9fa52ccc36
sustainablelobster <sustainablelobster@protonmail.com>
2025-02-23 08:27:16 -0800

    upgrade rla to .net 9 version

--- rla/update.ps1
+++ rla/update.ps1
@@ -13,7 +13,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $LatestReleasePage = Invoke-WebRequest -UseBasicParsing `
             -Uri 'https://raw.githack.com/EricZimmerman/ericzimmerman.github.io/master/index.md'
-    if ($LatestReleasePage.Content -notmatch 'RLA.*&vert;\s*\[([\d.]+)\]\((.*/net6/rla.zip)\)') {
+    if ($LatestReleasePage.Content -notmatch 'RLA.*&vert;\s*\[([\d.]+)\]\((.*/net9/rla.zip)\)') {
         throw "Unable to find tool info"
     }
 

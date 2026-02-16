9f5668ec5782f480c0c50fd1de55773d10c010a6
Chocolatey <chocolatey@realdimensions.net>
2026-02-16 16:01:50 +0000

    AU: 1 updated - fulleventlogview [skip ci] https://gist.github.com/sustainablelobster/9bec9a41eecea4b89447ad26780384ec/cd67528bac0185515a714925f9ee781b763405a9

--- fulleventlogview/tools/chocolateyinstall.ps1
+++ fulleventlogview/tools/chocolateyinstall.ps1
@@ -6,9 +6,9 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   url           = 'https://www.nirsoft.net/utils/fulleventlogview.zip'
   url64bit      = 'https://www.nirsoft.net/utils/fulleventlogview-x64.zip'
-  checksum      = 'b683bf400a967bd264d4f808c1c0269236a67bc853d8f222b7c71c25dff1b115'
+  checksum      = '1a9748e122bf48bbffa25c316851756150d41c886ab3b5a50a52b969a190afa2'
   checksumType  = 'sha256'
-  checksum64    = '020cb60e2e5a3f8b4d54d72bf45426516b74d194309115fbdf923aa159604b82'
+  checksum64    = 'dd67610f19a4fffe77460f55ebb318523cc8bcbcb8fc3d462ee0dffab4ba1d95'
   checksumType64= 'sha256'
 }
 
